899079eaa42fc5739660ffba66c57273a7280f85
Bitrus <josephbitrus62@gmail.com>
2023-04-08 12:50:04 +0100

    Initial Commit

---
+++ js/script-1.js
@@ -0,0 +1,8 @@
+// Typing animation 
+
+var typed = new Typed(".typing", {
+    strings:["", "Web Designer", "web Developer", "UI Designer", "Solar Engineer"],
+    typeSpeed: 120,
+    BackSpeed: 65,
+    loop:true
+})
